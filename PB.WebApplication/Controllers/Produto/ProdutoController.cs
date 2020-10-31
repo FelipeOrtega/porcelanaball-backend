0272673ccd8fc6c446a4e282743b67ac4d725e49
DiegoBarney <komyhu@hotmail.com>
2020-10-30 21:00:46 -0300

    Add api produto
    30/10/2020

---
+++ PB.WebApplication/Controllers/Produto/ProdutoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PB.WebApplication.Controllers.Produto
+{
+    [Route("Produto")]
+    public class ProdutoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(null);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(null);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Object inputModel)
+        {
+            return Ok(inputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Object inputModel)
+        {
+            return Ok(inputModel);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(null);
+        }
+    }
+}
