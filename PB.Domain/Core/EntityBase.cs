eb7c1cdd8da0c5c12d248e24cda64c9c951c3d76
Felipe Delboni Ortega <fdelb.ortega@gmail.com>
2020-12-05 16:02:42 -0300

    remoção  [DatabaseGenerated(DatabaseGeneratedOption.Identity)]

--- PB.Domain/Core/EntityBase.cs
+++ PB.Domain/Core/EntityBase.cs
@@ -5,7 +5,6 @@ namespace PB.Domain.Core
 {
     public abstract class EntityBase
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int codigo { get; set; }
     }
 }
