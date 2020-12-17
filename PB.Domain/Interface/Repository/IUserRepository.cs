ce0233c5f2d45cab174b5610074be990b784deab
DiegoBarney <komyhu@hotmail.com>
2020-12-16 23:18:01 -0300

    Retirado falso repositorio do user
    16/12/2020

--- PB.Domain/Interface/Repository/IUserRepository.cs
+++ PB.Domain/Interface/Repository/IUserRepository.cs
@@ -4,8 +4,6 @@ namespace PB.Domain.Interface.Repository
 {
     public interface IUserRepository : IRepositoryBase<User>
     {
-        public User Get_(string username, string password);
-
         public User Get(string username, string password);
 
     }
--- PB.InfraEstrutura/Data/Repository/UserRepository.cs
+++ PB.InfraEstrutura/Data/Repository/UserRepository.cs
@@ -13,14 +13,6 @@ namespace PB.InfraEstrutura.Data.Repository
         {
         }
 
-        public User Get_(string username, string password)
-        {
-            var users = new List<User>();
-            users.Add(new User { username = "batman", password = "batman", role = "manager" });
-            users.Add(new User { username = "robin", password = "robin", role = "employee" });
-            return users.Where(x => x.username.ToLower() == username.ToLower() && x.password == password).FirstOrDefault();
-        }
-
         public User Get(string username, string password)
         {
             User user = context.Set<User>().Where(x => x.username.ToLower() == username.ToLower() && x.password == password).FirstOrDefault();
