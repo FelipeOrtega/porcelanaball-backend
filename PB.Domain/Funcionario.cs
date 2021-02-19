63992b940012ff386227b0ad0936f7d31a83523b
aespina90 <contato@aespina.com>
2021-02-19 20:56:38 -0300

    Inclusão Dados Endereço para PESSOA - Backend
    endereco - string - varchar 100
    numero - string - varchar 10
    bairro - string - varchar 45
    cidade - string - varchar 45
    uf - string - char 2
    cep - string - varchar 10
    
    uf - string

--- PB.Domain/Funcionario.cs
+++ PB.Domain/Funcionario.cs
@@ -5,14 +5,7 @@ namespace PB.Domain
 {
     public class Funcionario : Pessoa
     {
-        public String rua { get; set; }
-        public String numero { get; set; }
-        public String bairro { get; set; }
-        public String cidade { get; set; }
-        public String uf { get; set; }
-        public String cep { get; set; }
         public String senha { get; set; }
-
         public List<FuncionarioPermissao> funcionarioPermissao { get; set; }
         public List<FuncionarioModalidade> modalidadeFuncionario { get; set; }
         public List<PlanoFuncionario> funcionarioPossuiPlano { get; set; }
--- PB.Domain/Pessoa.cs
+++ PB.Domain/Pessoa.cs
@@ -6,6 +6,13 @@ namespace PB.Domain
     public abstract class Pessoa : EntityBase
     {
         public String nome { get; set; }
+        public String endereco { get; set; }
+        public String numero { get; set; }
+        public String bairro { get; set; }
+        public String cidade { get; set; }
+        public String cep { get; set; }
+        public String uf { get; set; }
+        public String complemento { get; set; }
         public DateTime data_nascimento { get; set; }
         public String telefone_residencial { get; set; }
         public String telefone_celular { get; set; }
