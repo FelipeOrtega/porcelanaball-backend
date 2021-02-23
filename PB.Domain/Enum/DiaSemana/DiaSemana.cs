2b4876d3d55580e84fda83d00ea75d17a0c90655
Felipe Delboni Ortega <fdelb.ortega@gmail.com>
2021-02-23 00:14:36 -0300

    #16 Criação enum + endpoint listando descriptions

---
+++ PB.Domain/Enum/DiaSemana/DiaSemana.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PB.Domain
+{
+    public enum DiaSemana
+    {
+        [Description("Segunda-feira")]
+        SEGUNDA,
+
+        [Description("Terça-feira")]
+        TERCA,
+
+        [Description("Quarta-feira")]
+        QUARTA,
+
+        [Description("Quinta-feira")]
+        QUINTA,
+
+        [Description("Sexta-feira")]
+        SEXTA,
+
+        [Description("Sábado")]
+        SABADO,
+
+        [Description("Domingo")]
+        DOMINGO
+
+    }
+}
---
+++ PB.WebApplication/Controllers/DiaSemana/DiaSemanaController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using PB.Domain.Notifications;
+using PB.Service.Interface;
+using PB.WebApplication.Core;
+using Microsoft.AspNetCore.Authorization;
+using FluentValidation;
+using PB.Domain;
+using FluentValidation.Results;
+using X.PagedList;
+using System;
+using PB.Domain.Enum;
+using System.Linq;
+
+namespace PB.WebApplication.Controllers
+{
+    [Route("[controller]")]
+
+    public class DiaSemanaController : ApiBase
+    {
+
+        public DiaSemanaController(NotificationContext notificationContext) 
+        {
+            _notificationContext = notificationContext;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "manager, employee")]
+        public JsonReturn Get()
+        {
+           return ReturnJson(new DescriptionAttributes<DiaSemana>().Descriptions.ToList());
+        }
+
+    }
+}
